1ddba117cc0c6321a0bbfbe158551148dc04f818
RaRhAeu <rarha_eu@protonmail.com>
2025-02-05 16:52:10 +0100

    feat: global service, result backends removed

--- eventiq/settings.py
+++ eventiq/settings.py
@@ -1,10 +1,9 @@
-from typing import Any, Generic, Literal, Optional, TypeVar, Union
+from typing import Any, Generic, Optional, TypeVar, Union
 
 from pydantic.networks import AnyUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from .imports import ImportedType
-from .types import Decoder, DefaultAction, Encoder
+from .types import DefaultAction
 
 
 class BrokerSettings(BaseSettings):
@@ -23,13 +22,3 @@ Url = TypeVar("Url", bound=Union[AnyUrl, str])
 class UrlBrokerSettings(BrokerSettings, Generic[Url]):
     url: Url
     connection_options: dict[str, Any] = {}
-
-
-class ServiceSettings(BaseSettings):
-    name: str
-    title: Optional[str] = None
-    version: str = "0.1.0"
-    description: str = ""
-    asyncapi: Literal["3.0.0"] = "3.0.0"
-    encoder: Optional[ImportedType[Encoder]] = None
-    decoder: Optional[ImportedType[Decoder]] = None
